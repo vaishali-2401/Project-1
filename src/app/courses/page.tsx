2ea9a4b14489e1bed5c25f955359851c1ea19b7d
vaishali-2401 <vaishaligupta2401@gmail.com>
2025-07-10 01:39:06 +0530

    Update page.tsx

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -52,7 +52,7 @@ function page() {
                 <CardItem
                   translateZ={20}
                   as={Link}
-                  href="https://twitter.com/Sar_1707"
+                  href="https://twitter.com/"
                   target="__blank"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
